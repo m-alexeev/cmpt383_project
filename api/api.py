57b140f03eae5bd4401c10314e3fc51dd7f36f9e
B0uncyKnight <mishalxv35@gmail.com>
2020-09-13 23:39:37 -0700

    creating models

--- api/api.py
+++ api/api.py
@@ -1,8 +1,36 @@
-import time
+from datetime import datetime
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(120), unique = True, nullable = False)
+    image_file = db.Column(db.String(20), nullable = False, default = 'default.jpg')
+    password = db.Column(db.String(60), nullable = False)
+    notes = db.relationship('Note', backref = 'author', lazy = True)
+
+    def __repr__(self):
+        return f"User('{self.email}', '{self.image_file}')"
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+    title = db.Column(db.String(100), nullable = False)
+    date_created = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    content = db.Column(db.Text, nullable = False)
+
+    def __repr__(self):
+        return f"User('{self.title}', '{self.date_created}')"
+
+
+
 
 @app.route('/time')
 def get_cur_time():
-    return {'time': time.time()}
+    return {'time': datetime.utcnow}
