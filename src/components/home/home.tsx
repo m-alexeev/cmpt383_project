df011eb3018a8749a171b53d7dddffe2f321a722
B0uncyKnight <mishalxv35@gmail.com>
2020-09-13 23:40:25 -0700

    home

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import './home.css';
+import { useEffect, useState } from 'react';
 
 function HomePage() {
+
+  const [currentTime, setCurrentTime] = useState(1);
+
+
+  useEffect(()=> {
+    fetch('/time').then(res=> res.json()).then(data=>{
+      setCurrentTime(data.time);
+    });
+  }, [])
+
   return (
     <div className="Home">
       <header className="App-header">
         <p>
-          Welcome to the Home Page!
+          Welcome to the Home Page! 
+        </p>
+        <p>
+          The current time is {currentTime}
         </p>
       </header>
     </div>
