06143a73de418d23da0dda36c7e0805daae9f532
B0uncyKnight <mishalxv35@gmail.com>
2020-09-29 18:52:17 -0700

    Confirm Note delete

--- src/components/home/Stax/Card/Card.tsx
+++ src/components/home/Stax/Card/Card.tsx
@@ -1,14 +1,31 @@
 import './Card.css';
-import React from 'react'; 
+import React, { useState } from 'react'; 
+import Modal from 'react-bootstrap/esm/Modal';
+import Button from 'react-bootstrap/esm/Button';
+
+
+
+
 
 
 
 export default function Card(props){
  
+    const [show, setShow] = useState(false);
+
+    function handleClose(del){
+        if (del){
+            props.onDelete();
+        }
+        setShow(false);
+    } 
+
+    const handleShow = () => setShow(true); 
 
     let title = props.note.title;
     let txt = props.note.message; 
     let date = props.note.date; 
+
 
 
     return(
@@ -17,10 +34,11 @@ export default function Card(props){
                     {title}
                 </div>
                 <div className = 'stax-card-delete'>
-                    <img  
+                     <img
                         src = {process.env.PUBLIC_URL+ '/delete.svg'}
                         alt = "Delete Icon"
-                        onClick = {props.onDelete}></img>
+                        onClick= {()=>setShow(true)} >
+                    </img>
                 </div>
 
             <div className = 'stax-card-body'>
@@ -29,6 +47,23 @@ export default function Card(props){
             <div className = 'stax-card-footer'>
                 {date}
             </div>
+            
+            {/* Confirm Delete Dialog */}
+            <Modal show = {show} onHide = {handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirm Delete</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>You are about to delete this Note!</Modal.Body>
+                <Modal.Footer>
+                    <Button variant= 'danger' onClick={() => handleClose(true)}> 
+                        Delete
+                    </Button>
+                    <Button variant = 'primary'onClick = {() => handleClose(false)}>
+                        Cancel
+                    </Button>
+                </Modal.Footer>
+            
+            </Modal>
         </div>
     );
 }
--- src/components/home/Stax/CardStax.tsx
+++ src/components/home/Stax/CardStax.tsx
@@ -1,8 +1,9 @@
 import './CardStax.css';
 import React, { useState } from 'react'
-import { Button } from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
 import Card from './Card/Card';
-
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import { Dialog } from 'react-bootstrap/lib/Modal';
 
 class Note {
     title: string;
@@ -17,15 +18,12 @@ class Note {
 }
 
 
-
-
 export default function CardStax() {
 
     let date = new Date();
     let note1 = new Note("Note1", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum venenatis neque. Nulla metus nulla, varius eu est eu, ornare congue nulla. Maecenas et quam commodo, suscipit libero ut, ultricies nisi. Integer lectus tellus, molestie eu commodo at, pellentesque nec justo.", date.toUTCString());
     let note2 = new Note("Note2", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum venenatis neque. Nulla metus nulla, varius eu est eu, ornare congue nulla. Maecenas et quam commodo, suscipit libero ut, ultricies nisi. Integer lectus tellus, molestie eu commodo at, pellentesque nec justo.", date.toUTCString());
     let note3 = new Note("Note3", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", date.toUTCString()); 
-    //   const [cards , setCards ] = useState([]); 
 
     const  [notes, setNotes] = useState([note1,note2,note3]); 
 
@@ -35,18 +33,27 @@ export default function CardStax() {
         setNotes(tempNotes);
     }
 
+
+    const items = notes.map((note,index)=><Card  key = {index} note = {note} onDelete= {()=>deleteNote(index)}/>);
+
+
     return (
         <div className='card-stax'>
-                <Button onClick = {() => setNotes( [...notes, note1]) } >Create Card</Button>
-                <div className="container-fluid d-flex ">
-                    <div className = 'row'>
-                        {notes.map((note,index) =>{
-                            // return <Card  key = {index} title = {note.title} txt = {note.message} date = {note.date} />
-                            return <Card  key = {index} note = {note} onDelete= {()=>deleteNote(index)}/>
-                        })
-                        }
-                    </div>
-                </div>
+            <Button onClick = {() => setNotes( [...notes, note1]) } >Create Card</Button>
+            <div className="container-fluid d-flex ">
+                <ReactCSSTransitionGroup
+                    className = 'row'
+                    transitionAppearTimeout = {300}
+                    transitionEnterTimeout = {300}
+                    transitionLeaveTimeout = {300}
+                    transitionName="example"
+                    transitionAppear={true}
+                    transitionEnter={true}
+                    transitionLeave={true}
+                >
+                    {items}
+                </ReactCSSTransitionGroup>
+            </div>
         </div>
     )
 }
