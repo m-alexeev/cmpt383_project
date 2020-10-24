2fe3f3d1924d4c4bf9d6e4afb1e98d100a558884
B0uncyKnight <mishalxv35@gmail.com>
2020-10-23 23:33:53 -0700

    setting up friends page

--- react-front/src/components/home/friends/friends.tsx
+++ react-front/src/components/home/friends/friends.tsx
@@ -1,30 +1,94 @@
-import React, { useEffect, useState } from 'react'; 
+import React, { useEffect, useState } from 'react';
+import { Form } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
 import NavBar from '../Navbar/navbar'
 
+import './friends.css'
 
 
-export default function Friends (){
+class FriendObj{
+   id: number;
+   username: string; 
+   img: string;
 
-    const history = useHistory(); 
-    const [curUser, setCuruser] = useState("");
+   constructor(id:number, username:string, img:string){
+      this.id = id;
+      this.username = username; 
+      this.img = img; 
+   }
+}
 
-    useEffect(() => {
-        fetch('/getUser').then(res => res.json()).then(data => {
-          setCuruser(data.user);
-          if (data.redirect != null) {
+
+
+
+export default function Friends() {
+
+   const history = useHistory();
+   const [curUser, setCuruser] = useState("");
+   const [friends, setFriends] = useState([])
+
+   const [search, setSearch] = useState("");
+
+
+   //TODO: Create user obj 
+
+   useEffect(() => {
+      fetch('/getUser').then(res => res.json()).then(data => {
+         setCuruser(data.user);
+         if (data.redirect != null) {
             history.push('/login');
-          }
-        });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-     }, []);
+         }
+      });
+      //TODO: Create search query 
+      console.log(search);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, [search]);
 
 
-    return (
-       <div className='friends-container'>
-           <NavBar user = {curUser}/>
-            
-       </div>
-    )
-    }
-    
+   return (
+      <div className='main-container'>
+         <NavBar user={curUser} />
+         <div className='friends-container'>
+            <div className='friends-list'>
+               List
+                </div>
+            <div className='friends-search'>
+               <div className='search-form'>
+                  <Form>
+                     <Form.Group>
+                        <Form.Control
+                           type='text'
+                           placeholder='Enter Username'
+                           onChange={e => setSearch(e.target.value)}>
+                        </Form.Control>
+                     </Form.Group>
+                  </Form>
+               </div>
+            </div>
+         </div>
+      </div>
+   )
+}
+
+
+
+export function Friend(props){
+
+   const username = props.username; 
+   const id = props.id;
+   const img = props.img;
+ 
+   return (
+      <div className = "box">
+         <div className = 'pic'>
+            <img src={img}></img>
+         </div>
+         <div className = 'username'>
+            {username}
+         </div>
+         <div className = 'add-button'>
+            +
+         </div>
+      </div>
+   )
+}
